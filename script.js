4959f1732fc12ce42683656df5dae43e259d4b3c
keombre <keombre8@gmail.com>
2019-05-18 00:04:06 +0200

    small touchups

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-        return {cancel: true};
-    },
-    {
-        urls: ["*://progtest.fit.cvut.cz/css.css"]
-    },
+    function(details) {return {cancel: true};},
+    {urls: ["*://progtest.fit.cvut.cz/css.css"]},
     ["blocking"]
 );
