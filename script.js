6a3e708952843c0bf6a64d661cd06868659728b5
keombre <keombre8@gmail.com>
2019-05-17 23:21:52 +0200

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return {cancel: true};
+    },
+    {
+        urls: ["*://progtest.fit.cvut.cz/css.css"]
+    },
+    ["blocking"]
+);
