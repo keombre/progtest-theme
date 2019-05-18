ea85c8fa7bd25bc4c3a387992f7f63e1e61ee387
keombre <keombre8@gmail.com>
2019-05-18 16:17:44 +0200

    fixed navbar

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
-window.onload = () => {
-    chrome.runtime.sendMessage({type: "theme"}, function(response) {
-        if (response.theme == 'orig')
-            return
+chrome.runtime.sendMessage({type: "theme"}, function(response) {
+    if (response.theme == 'orig')
+        return
+    window.onload = () => {
         let styles = ''
-
+    
         // add selector to duplicate parrent elements
         document.querySelectorAll("span.dupC").forEach(e => {e.parentNode.className += " dupCpar"})
-
+    
         // mark number of columns
         let c = [], i = 0
         let qsel = document.querySelector("tr.resHdr:nth-child(1)")
@@ -17,13 +17,24 @@ window.onload = () => {
             c.forEach(e => {
                 styles += 'body > center > table tr.resRow > td:nth-child('+ (e + 1) +') {border-left: thin solid rgba(0, 0, 0, 0.25);font-weight: 500;}'
             })
-
+    
             styles += 'body > center > table tr.resRow > td:last-child {font-weight: 500;}'
         }
-
+    
         let styleSheet = document.createElement("style")
         styleSheet.type = "text/css"
         styleSheet.innerText = styles
         document.head.appendChild(styleSheet)
-    })
-}
+
+        let header = document.querySelector("body > table")
+
+        if (typeof header != "undefined" && header != null) {
+            window.onscroll = () => {
+                if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40)
+                    header.style.padding = "0px 16px";
+                else
+                    header.removeAttribute("style")
+            }
+        }
+    }
+})
