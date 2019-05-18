3f58784271cb326d2e2c7fb83b23eff8500b7287
keombre <keombre8@gmail.com>
2019-05-19 01:26:53 +0200

    ready for dark rewrite

--- background.js
+++ background.js
@@ -10,15 +10,12 @@ chrome.runtime.onInstalled.addListener(function () {
     });
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [new chrome.declarativeContent.PageStateMatcher({
-                    pageUrl: { hostEquals: 'progtest.fit.cvut.cz' },
-                })
-                ],
-                actions: [new chrome.declarativeContent.ShowPageAction()]
-            }
-        ]);
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: { hostEquals: 'progtest.fit.cvut.cz' },
+            })],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
     });
 });
 
