393e7437e7dfb895ede7752a624dfc7282aaed79
keombre <keombre8@gmail.com>
2020-01-28 17:43:53 +0100

    touchups

--- src/content/end.js
+++ src/content/end.js
@@ -544,7 +544,7 @@ class Task extends Logged {
                     multElem.innerText = scoreMult.toFixed(2)
                     scoreElem.appendChild(multElem)
                 }
-                else if (f.innerText.includes('Celková doba běhu:')) {
+                else if (f.innerText.includes('Celková doba běhu:') || f.innerText.includes("Vyčerpání limitu na celý test, program násilně ukončen")) {
                     const time = [...f.innerText.matchAll(/(\d+.\d+)/g)]
                     if (time.length != 0) {
                         markForRemove.push(f)
